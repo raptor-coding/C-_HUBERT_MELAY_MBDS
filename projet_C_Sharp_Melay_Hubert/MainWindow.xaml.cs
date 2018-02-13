3b1ea7770b0a6540d741e09a44cbbf5edc2f68d7
Julien <julien2105@gmail.com>
2018-02-13 11:35:12 +0100

    fenêtre d'accueil avec les boutons et la saisie du nickname

--- projet_C_Sharp_Melay_Hubert/MainWindow.xaml.cs
+++ projet_C_Sharp_Melay_Hubert/MainWindow.xaml.cs
@@ -20,9 +20,22 @@ namespace projet_C_Sharp_Melay_Hubert
     /// </summary>
     public partial class MainWindow : Window
     {
+        static String nameUser;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        void EnterClicked(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                nameUser = new TextRange(richTextBox1.Document.ContentStart, richTextBox1.Document.ContentEnd).Text;
+                label1.Content += " " + nameUser;
+                Console.Write(nameUser);
+                e.Handled = true;
+            }
+        }
+
     }
 }
