82a585c6aff9a1314b9f422123e813d182986c34
Julien <julien2105@gmail.com>
2018-02-15 23:47:01 +0100

    Affichage d'autant de graines dans les pits que de graines présentent dans l'objet Pit + nombre de graines affichée sous le Pit.

--- projet_C_Sharp_Melay_Hubert/boardGameGUI.xaml.cs
+++ projet_C_Sharp_Melay_Hubert/boardGameGUI.xaml.cs
@@ -26,20 +26,43 @@ namespace projet_C_Sharp_Melay_Hubert
 
         public void updateGUI(BoardGame boardgame)
         {
-            //DependencyObject child = VisualTreeHelper.GetChild(GridGame, );
+            // Array des Canvas à MAJ en fonction des graines dedans
+            List<Canvas> canvas = new List<Canvas>(12); 
+            canvas.Add(Col0Row0); canvas.Add(Col1Row0); canvas.Add(Col2Row0); canvas.Add(Col3Row0); canvas.Add(Col4Row0); canvas.Add(Col5Row0);
+            canvas.Add(Col0Row1); canvas.Add(Col1Row1); canvas.Add(Col2Row1); canvas.Add(Col3Row1); canvas.Add(Col4Row1); canvas.Add(Col5Row1);
 
             for (int k = 0; k < boardgame.Players.Count; k++)
             {
                 List<Pit> pits = boardgame.Players[k].Pits;
                 for (int i = 0; i < pits.Count; i++)
                 {
-                    for (int j = 1; j < pits[i].Seeds ; j++)
+                    for (int j = 1; j < pits[i].Seeds+1 ; j++)
                     {
-                        DependencyObject child = VisualTreeHelper.GetChild(Col0Row0, j);
+                        DependencyObject child;
+                        if (k > 0)
+                        {
+                            child = VisualTreeHelper.GetChild(canvas[i+pits.Count], j);
+                        }
+                        else
+                        {
+                            child = VisualTreeHelper.GetChild(canvas[i], j);
+                        }
                         Ellipse e = (Ellipse)child;
-                        Console.WriteLine(e.Fill);
                         e.Visibility = System.Windows.Visibility.Visible;
                     }
+                    DependencyObject labelCount;
+                    if (k > 0)
+                    {
+                        labelCount = VisualTreeHelper.GetChild(canvas[i + pits.Count], 16);
+                    }
+                    else
+                    {
+                        labelCount = VisualTreeHelper.GetChild(canvas[i], 16);
+                    }
+                    Label l = (Label)labelCount;
+                    l.Content = pits[i].Seeds;
+
+
                 }
             }
         }
